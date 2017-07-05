eb0bca8fae4665c2d2a631ce0b4bb89be550542d
diorw <406714601@qq.comgit config --global user.name diorw;quitgit config --global user.email 406714601@qq.com>
2017-07-05 14:44:41 +0800

    日期含有任务加小点

--- app/src/main/java/com/example/wangchang/testbottomnavigationbar/fragment/MusicFragment.java
+++ app/src/main/java/com/example/wangchang/testbottomnavigationbar/fragment/MusicFragment.java
@@ -35,7 +35,11 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
 import java.util.List;
 
 import static android.support.v7.widget.StaggeredGridLayoutManager.TAG;
@@ -49,46 +53,70 @@ public class MusicFragment extends Fragment {
     private RecyclerView recyclerView;
     MaterialCalendarView widget;
     public List<TaskBeen> tasks;
+    public List<TaskBeen> alltasks;
     private TaskBeenDao taskBeenDao;
     private String now;
     private String[] tasktitle = {"背单词","给XXX打电话"};
     private Boolean[] iscomplete = {true,true};
     private TaskAdapter taskAdapter;
+    private HashSet<CalendarDay> hashset = new HashSet<>();
+    private Date datetemp;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.datetasklayout, container, false);
         recyclerView  = (RecyclerView)view.findViewById(R.id.taskrecycleview);
         widget = (MaterialCalendarView)view.findViewById(R.id.calendarView);
-
+        DaoMaster.DevOpenHelper devopenhelper = new DaoMaster.DevOpenHelper(getContext(),"task-db",null);
+        DaoMaster daomaster = new DaoMaster(devopenhelper.getWritableDatabase());
+        DaoSession daosession = daomaster.newSession();
+        taskBeenDao = daosession.getTaskBeenDao();
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        DaoMaster.DevOpenHelper devopenhelper = new DaoMaster.DevOpenHelper(getContext(),"task-db",null);
-        DaoMaster daomaster = new DaoMaster(devopenhelper.getWritableDatabase());
-        DaoSession daosession = daomaster.newSession();
-        taskBeenDao = daosession.getTaskBeenDao();
-        TaskBeen newTask = new TaskBeen(2,"2017-7-6",null,null,null,false,null,"约饭");
-        taskBeenDao.insert(newTask);
+    //   taskBeenDao.deleteAll();
+        datetemp= new Date();
+        SimpleDateFormat dateformate = new SimpleDateFormat("yyyy-MM-dd");
+        String now =dateformate.format(datetemp);
+        Log.d("nowtime", "onActivityCreated: "+now);
+      /*  TaskBeen newTask = new TaskBeen(1,now,null,null,null,false,null,"约饭2");
+        taskBeenDao.insert(newTask);*/
+        //给含有任务的天加绿点
+        alltasks = taskBeenDao.loadAll();
+     //   Log.d("tasklenth", "onActivityCreated: "+alltasks.size());
+        for(int i = 0;i<alltasks.size();i++){
+            String nowday = alltasks.get(i).getDate();
+
+            String[] data= nowday.split("-");
+
+            CalendarDay temp = (CalendarDay.from(Integer.valueOf(data[0]),Integer.valueOf(data[1])-1,Integer.valueOf(data[2])));
+            Log.d("day", "onActivityCreated: "+data[0]);
+            Log.d("day", "onActivityCreated: "+data[1]);
+            Log.d("day", "onActivityCreated: "+data[2]);
+            if(!hashset.contains(temp)){
+                hashset.add(temp);
+            }
+
+        }
+      //  Log.d("hashset", "onActivityCreated: "+hashset.size());
         recyclerView.setLayoutManager(new WrapContentLinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        tasks = taskBeenDao.queryBuilder().where(TaskBeenDao.Properties.Date.eq("2017-7-1")).build().list();
+        tasks = taskBeenDao.queryBuilder().where(TaskBeenDao.Properties.Date.eq(now)).build().list();
         taskAdapter = new TaskAdapter(tasks);
         recyclerView.setAdapter(taskAdapter);
-       // widget.addDecorators(new EventDecorator(R.color.green,hashset));
-       widget.setOnDateChangedListener(new OnDateSelectedListener() {
+        widget.addDecorators(new EventDecorator(R.color.green,hashset));
+        widget.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                String temp = date.toString();
-                now = temp.substring(temp.lastIndexOf("{")+1,temp.lastIndexOf("}"));
+                java.sql.Date sdate = new java.sql.Date(date.getDate().getTime());
                 tasks.clear();
-                tasks.addAll(taskBeenDao.queryBuilder().where(TaskBeenDao.Properties.Date.eq(now)).build().list());
+                tasks.addAll(taskBeenDao.queryBuilder().where(TaskBeenDao.Properties.Date.eq(sdate.toString())).build().list());
                 taskAdapter.notifyDataSetChanged();
 
-                Log.d("change", "onDateSelected: "+tasks.size());
-                //Toast.makeText(getContext(), now,Toast.LENGTH_LONG).show();
+                Log.d("change", "onDateSelected: "+sdate.toString());
+            //    Toast.makeText(getContext(),date.getDate().toString(),Toast.LENGTH_LONG).show();
             }
         });
         taskAdapter.setonItemClickListener(new TaskAdapter.OnItemClickListener() {
