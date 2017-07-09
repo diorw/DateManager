d5a8ef073866e0fb4823ea1f8671148468edb998
xuwenkai357 <1002699636@qq.com>
2017-07-09 11:19:06 +0800

    Account各个界面

---
+++ app/src/main/java/com/example/wangchang/wda1150_xwk1151/MonthAccountActivity.java
@@ -0,0 +1,21 @@
+package com.example.wangchang.wda1150_xwk1151;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dell-pc on 2017/7/9.
+ */
+
+public class MonthAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
